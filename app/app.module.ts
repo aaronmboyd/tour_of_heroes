0b8f72b1e54bff2601d5f924f19c09f651f27414
Aaron Boyd <aaronmboyd@gmail.com>
2016-12-14 23:09:48 +1100

    Initial routing changes

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,38 @@ import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroesComponent }		  from './heroes.component';
+import { DashboardComponent }   from './dashboard.component'
+import { RouterModule }         from '@angular/router';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, 
-                  FormsModule],
+  imports:  [ BrowserModule, 
+              FormsModule,
+              RouterModule.forRoot([
+                {
+                  path : 'heroes',
+                  component: HeroesComponent
+                },
+                {
+                  path: 'dashboard',
+                  component: DashboardComponent
+                },
+                {
+                  path: '',
+                  redirectTo: '/dashboard',
+                  pathMatch: 'full'
+                }
+              ])
+            ] 
+,
   declarations: [ AppComponent,
-                  HeroDetailComponent ],
+                  DashboardComponent,
+                  HeroDetailComponent,
+                  HeroesComponent],
+  providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
