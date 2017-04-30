0ece0784ebd27f2066b46c77fab45a407225277a
Aaron Boyd <aaronmboyd@gmail.com>
2017-04-30 14:21:08 +1000

    Added 'Save' button and functionality for persistance

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -24,16 +24,27 @@ export class HeroService{
 	}
 
 	getHero(id: number): Promise<Hero> {
-	const url = `${this.heroesUrl}/${id}`;
-	return this.http.get(url)
+		const url = `${this.heroesUrl}/${id}`;
+		return this.http.get(url)
 		.toPromise()
 		.then(response => response.json().data as Hero)
 		.catch(this.handleError);
 	}
 
   	private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
+    	console.error('An error occurred', error); // for demo purposes only
+    	return Promise.reject(error.message || error);
+  	}
+
+	private headers = new Headers({'Content-Type': 'application/json'});
+
+	update(hero: Hero): Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+		return this.http
+		.put(url, JSON.stringify(hero), {headers: this.headers})
+		.toPromise()
+		.then(() => hero)
+		.catch(this.handleError);
+	}
 
 }
